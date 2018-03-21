274738ba6b9dfff7ecec363d563c716bd43f53ec
TrickHunter <trickhunter.tk@gmail.com>
2018-03-21 10:52:26 +0000

    2nd day nd upolad

---
+++ 2.rb
@@ -0,0 +1,24 @@
+# assiging value of a
+a = 10 
+
+# Doing Some Mathematical Problems
+b = 7.91* a + 2 
+
+# use puts or print instead of printf
+printf("%d & %f\n", a, b);
+
+# Type is dynamic.
+b = "A string"
+c = 'Another String'
+print b + " and " + c + "\n"
+a= "vikash".reverse
+#We Can't Reverse a Interger Value So Converting that integer vaule to string
+b= 456.to_s.reverse
+
+# You may Use Puts too
+print a,"\n"
+puts b
+
+# we can directly reverse string value
+d= 'yati'.reverse!
+puts d
